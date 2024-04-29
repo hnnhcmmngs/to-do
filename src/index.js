7828d1a568e881c27cf3a110f39591052968cb00
hnnhcmmngs <hannahcummings94@gmail.com>
2024-04-28 22:55:36 -0400

    Create js module to manage to do tasks
    A module has been created to store the to do tasks and provide a
    method to create new tasks. It contains a class definition for to
    do items. The module is imported into index.js and tested using some
    dummy content and console log statements.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+import toDos from './todo';
+
+console.log(toDos.tasks);
+toDos.addTask("hello", "hi", "never", "low");
+console.log(toDos.tasks);
---
+++ src/todo.js
@@ -0,0 +1,24 @@
+const toDos = (function() {
+    const tasks = [];
+
+    class ToDo {
+        constructor(title, description, dueDate, priority) {
+            this.title = title;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.priority = priority;
+        }
+    }
+
+    const addTask = (title, description, dueDate, priority) => {
+        const newTask = new ToDo(title, description, dueDate, priority);
+        tasks.push(newTask);
+    }
+
+    return {
+        tasks,
+        addTask
+    }
+})();
+
+export default toDos;
